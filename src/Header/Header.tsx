d131d642f3258c7432756a0caa245be67d13ab2a
Faizan Husain <114300196+FaizanHusainn@users.noreply.github.com>
2025-03-01 20:50:49 +0530

    second commit

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
 import { IconBriefcase } from '@tabler/icons-react';
+import { IconBellRinging } from '@tabler/icons-react';
+import { IconSettings } from '@tabler/icons-react';
+import { Avatar, NavLink } from '@mantine/core';
+import { Indicator } from '@mantine/core';
+import NavLinks from './NavLinks';
+import avatar from "../assets/avatar.png";
 
 const Header = () => {
   return (
     <div className='w-full bg-black h-20 px-6 text-white flex justify-between items-center'>
-        <div className='flex gap-3 items-center'>
+        <div className='flex gap-3 items-center text-[#ffbd20] text-3xl'>
             <IconBriefcase stroke={1.25} className='h-8 w-8'/> 
             iJobs
         </div>
-        <div> links</div>
-        <div> profile</div>
+        {/* Calling the naving method from the NavLinks file */}
+        {NavLinks()}
+        <div className='flex items-center gap-4'>
+        <div className='flex items-center gap-2'>
+            <div>Marshall</div>
+            <Avatar src={avatar} alt="it's me"  />
+        </div>
+        <div className='bg-[#3d3d3d] p-1.5 rounded-full'>
+          <IconSettings stroke={1.5} />
+        </div>
+        <div className='bg-[#3d3d3d] p-1.5 rounded-full'>
+
+           {/* Mantine indicator to show notification on bell icon */}
+          <Indicator processing color='brightSun.4' offset={6} size={8}>
+            <IconBellRinging stroke={1.5} />
+          </Indicator>
+        </div>
+        </div>
     </div>
   )
 }
---
+++ src/Header/NavLinks.tsx
@@ -0,0 +1,13 @@
+
+const NavLinks = () => {
+  return (
+    <div className='flex gap-6'>
+    <a href="">Find Jobs</a>
+    <a href="">Find Talent</a>
+    <a href="">Upload Job</a>
+    <a href="">About Us</a>
+</div>
+  )
+}
+
+export default NavLinks
